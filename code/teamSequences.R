622ebaf5bda7f50ccb04906230a3bc6ec59ea610
pssguy <agcur@rogers.com>
2015-10-30 17:48:17 -0700

    Team sequencwes input added venue and opponent options

--- code/teamSequences.R
+++ code/teamSequences.R
@@ -1,7 +1,10 @@
+## add current
 
+## sort venue link
 
 
 data <- eventReactive(input$seq_Button,{
+  if (input$seq_Opp=="All") {
   home <- df %>% 
     filter(home==input$seq_Team) %>% 
     mutate(gameOrder=row_number(),win=ifelse(result=="H",1,0),
@@ -14,50 +17,108 @@ data <- eventReactive(input$seq_Button,{
            loss=ifelse(result=="H",1,0),nowin=ifelse(result!="A",1,0),noloss=ifelse(result!="H",1,0),
            team=visitor)
   
-  all <- rbind(home,away) %>% 
+  allGames <- rbind(home,away) %>% 
     arrange(gameDate) %>% 
     mutate(gameOrder=row_number())
+  } else {
+    home <- df %>% 
+      filter(home==input$seq_Team&visitor==input$seq_Opp) %>% 
+      mutate(gameOrder=row_number(),win=ifelse(result=="H",1,0),
+             loss=ifelse(result=="A",1,0),nowin=ifelse(result!="H",1,0),noloss=ifelse(result!="A",1,0),
+             team=home)
+    
+    away <- df %>% 
+      filter(visitor==input$seq_Team&home==input$seq_Opp) %>% 
+      mutate(gameOrder=row_number(),win=ifelse(result=="A",1,0),
+             loss=ifelse(result=="H",1,0),nowin=ifelse(result!="A",1,0),noloss=ifelse(result!="H",1,0),
+             team=visitor)
+    
+    allGames <- rbind(home,away) %>% 
+      arrange(gameDate) %>% 
+      mutate(gameOrder=row_number())
+  }  
   
-  if (input$seq_category=="Win") {
-  df_seq <- all %>% 
+  print(glimpse(allGames))
+  
+  if (input$seq_Venue=="All") {
+  if (input$seq_Category=="Win") {
+  df_seq <- allGames %>% 
     select(team,gameDate,goaldif,win) %>%
     do (subSeq(.$win)) %>% 
     filter(value==1) %>% 
     mutate(gameOrder=as.integer(first))
-  } else if (input$seq_category=="No Win"){
-    df_seq <- all %>% 
+  } else if (input$seq_Category=="No Win"){
+    df_seq <- allGames %>% 
       select(team,gameDate,goaldif,win) %>%
       do (subSeq(.$win)) %>% 
       filter(value==0) %>% 
       mutate(gameOrder=as.integer(first))
-  } else if (input$seq_category=="Loss"){
-    df_seq <- all %>% 
+  } else if (input$seq_Category=="Loss"){
+    df_seq <- allGames %>% 
       select(team,gameDate,goaldif,loss) %>%
       do (subSeq(.$loss)) %>% 
       filter(value==1) %>% 
       mutate(gameOrder=as.integer(first))
-  } else if (input$seq_category=="No Loss"){
+  } else if (input$seq_Category=="No Loss"){
    
-    df_seq <- all %>% 
+    df_seq <- allGames %>% 
       select(team,gameDate,goaldif,loss) %>%
       do (subSeq(.$loss)) %>% 
       filter(value==0) %>% 
       mutate(gameOrder=as.integer(first))
   }
+  } else if (input$seq_Venue=="Home") {
+    if (input$seq_Category=="Win") {
+      df_seq <- home %>% 
+        select(team,gameDate,goaldif,win) %>%
+        do (subSeq(.$win)) %>% 
+        filter(value==1) %>% 
+        mutate(gameOrder=as.integer(first))
+    } else if (input$seq_Category=="No Win"){
+      df_seq <- home %>% 
+        select(team,gameDate,goaldif,win) %>%
+        do (subSeq(.$win)) %>% 
+        filter(value==0) %>% 
+        mutate(gameOrder=as.integer(first))
+    } else if (input$seq_Category=="Loss"){
+      df_seq <- home %>% 
+        select(team,gameDate,goaldif,loss) %>%
+        do (subSeq(.$loss)) %>% 
+        filter(value==1) %>% 
+        mutate(gameOrder=as.integer(first))
+    } else if (input$seq_Category=="No Loss"){
+      
+      df_seq <- home %>% 
+        select(team,gameDate,goaldif,loss) %>%
+        do (subSeq(.$loss)) %>% 
+        filter(value==0) %>% 
+        mutate(gameOrder=as.integer(first))
+    }
+  }
   
-  info=list(df_seq=df_seq,all=all)
+  info=list(df_seq=df_seq,allGames=allGames,away=away,home=home)
   return(info)
   
 })
 
 output$teamSeqs <- renderDimple({
   
-  if (is.null( data()$df_seq)) return()
+  if (is.null( data()$df_seq)) return() 
   
+  #print(glimpse( data()$df_seq))
+  
+  if (input$seq_Venue=="All") {
   data()$df_seq %>% 
     filter(slength>=input$seq_Run) %>% 
-    left_join(data()$all) %>% 
+    left_join(data()$allGames) %>% 
     rename(Sequence=slength) %>%
     dimple(x="gameDate",y="Sequence",type="bar")#
+  } else if (input$seq_Venue=="Home") {
+    data()$df_seq %>% 
+      filter(slength>=input$seq_Run) %>% 
+      left_join(data()$home) %>% 
+      rename(Sequence=slength) %>%
+      dimple(x="gameDate",y="Sequence",type="bar")#
+  } 
   
 })
